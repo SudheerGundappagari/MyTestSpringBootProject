44b21f02347532c42c97b1e4ca5b9ffc40cb6875
sgundappagari <sgundappagari@AIPL4630LT11336.altimetrik.com>
2021-06-10 12:13:13 +0530

    First COmmit

---
+++ src/main/java/com/mytestspringboot/demo/MyTestSpringBootProjectApplication.java
@@ -0,0 +1,13 @@
+package com.mytestspringboot.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyTestSpringBootProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyTestSpringBootProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mytestspringboot/demo/MyTestSpringBootProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mytestspringboot.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyTestSpringBootProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
